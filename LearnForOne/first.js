b933ea21950632dec6c740fcee973417a7c4af5e
ymanish1475-del <ymanish1475@gmail.com>
2026-05-07 18:33:45 +0530

    new injs

--- LearnForOne/first.js
+++ LearnForOne/first.js
@@ -1,3 +1,46 @@
 // console.log("man1");
 // console.log("manish ka js project");
-console.log("manish ka  js project");
+// console.log("manish ka  js project");
+// let arr=[4,6,3,6,false,"manish yadav"]
+
+// for (let a in arr){
+//     console.log(a,"in")
+// }
+// for (let a of arr){
+//     console.log(a,"of")
+// }
+
+// let student ={
+//     id:1,
+//     name:"manish yadav"
+
+// }
+// console.log(student.id);
+
+// global execution context
+
+// memeory creation , code execution
+// sum()
+// let a= 7;
+
+
+console.log(a)
+var a=89;
+
+
+
+// function sum(){
+//     console.log(a)
+// }
+
+// function outer(){
+
+//     console.log(a)
+//     function inner(){
+//         console.log("hiii");
+//     }
+
+//     inner()
+// }
+// outer()
+
