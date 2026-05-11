45318517b8d919172ae0041e469b0c9e7ebb293a
ymanish1475-del <ymanish1475@gmail.com>
2026-05-11 12:48:55 +0530

    dom manupulation

---
+++ Dom/index.js
@@ -0,0 +1,14 @@
+
+
+
+console.log("wooooo")
+
+let btn=document.querySelector("#one")
+let body=document.querySelector(".two")
+
+btn.addEventListener("click",function(){
+    body.style.backgroundColor="red"
+    console.log("hello")
+})
+// let a= document.querySelector("button")
+// a.addEventListener
