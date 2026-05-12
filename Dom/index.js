d78b04ffe1d8fbd333d184b7b838ce8e03ffe2fa
ymanish1475-del <ymanish1475@gmail.com>
2026-05-12 11:23:38 +0530

    event listner

--- Dom/index.js
+++ Dom/index.js
@@ -1,15 +1,46 @@
 
 
 
-console.log("wooooo")
+// console.log("wooooo")
 
-// let btn=document.querySelector("#one")
-let btn=document.getElementById("one")
-let body=document.querySelector(".two")
+// // let btn=document.querySelector("#one")
+// let btn=document.getElementById("one")
+// let body=document.querySelector(".two")
 
-btn.addEventListener("click",function(){
-    body.style.backgroundColor="pink"
-    console.log("hello")
-})
+// btn.addEventListener("click",function(){
+//     body.style.backgroundColor="pink"
+//     console.log("hello")
+// })
 // let a= document.querySelector("button")
 // a.addEventListener
+
+// let h= document.getElementsByClassName("h")
+let h= document.querySelectorAll("h1")
+
+let h3= document.querySelector("h3")
+// let name=prompt("enter your name")
+h3.innerText="name :"+"  "+name
+
+let inp=document.querySelector("input")
+let h2=document.querySelector("h2")
+
+// inp.addEventListener("input",function(z){
+//     h2.innerText=z.target.value
+// })
+
+let btn=document.querySelector("button")
+let h4=document.querySelector("h4")
+
+btn.addEventListener("click",function(){
+    console.log("hello");
+    let data=inp.value
+    h4=data
+
+
+})
+
+// h[0].style.color="green"
+
+// for(let i of  h1){
+//     h[i].style.color="blue"
+// }
