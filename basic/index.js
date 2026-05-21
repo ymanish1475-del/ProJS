049f07b6139a2dff66ff2fd6c5b4dc2691a9e96f
ymanish1475-del <ymanish1475@gmail.com>
2026-05-21 15:17:51 +0530

    basic

---
+++ basic/index.js
@@ -0,0 +1,37 @@
+
+// let arr=[4,6,4,2,5,3,]
+// let num =[...arr,8,5,3,,4]    //sppeard operation
+// // console.log(num);
+
+// let nums=num.concat(arr)
+// // console.log(nums);
+
+// let obj={
+//     id:1,
+//     name:"manish yadav"
+// }
+// let data={
+//     ...obj,
+//     age:19
+// }
+
+// // console.log(data)
+
+// function sum(a,b,c,...nums){   //rest parametere 
+//     console.log()
+// }
+// // console.log(sum());
+// sum(3,4,5,7,5,8,9,)
+
+let arr=[3,5,44,3,3,4]
+let [a,b,c]=arr
+console.log([a,b,c]);
+
+let obj={
+    id:1,
+    name:"manish yadav"
+}
+
+let {id,name}=obj
+
+console.log({id,name})
